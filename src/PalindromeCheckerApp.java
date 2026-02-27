2b556bdeb987b5c0d9ba235a978a481ece207728
Netraveejay <netraveejay2006@gmail.com>
2026-02-27 09:25:42 +0530

    Netra Added UC3: Palindrome Check Using String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,15 +4,15 @@ public class PalindromeCheckerApp {
         Scanner sc=new Scanner(System.in);
         System.out.print("Input text:");
         String input=sc.nextLine();
+        String rev="";
         boolean isPalindrome=true;
-        for(int i=0;i<input.length()/2;i++){
+        for(int i=input.length()-1;i>=0;i--){
             if(input.charAt(i)!=input.charAt(input.length()-1-i)){
-                isPalindrome=false;
-                break;
+                rev=rev+input.charAt(i);
             }
 
         }
-        if(isPalindrome){
+        if(input.equals(rev)){
             System.out.println("Is it a palindrome?: true");
         }else{
             System.out.println("Is it a palindrome?: false");
